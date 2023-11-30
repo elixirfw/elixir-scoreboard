8b593a08a589538697dacbddb730152c99ec9f24
Elixir <96543010+elixirfw@users.noreply.github.com>
2023-12-01 03:51:33 +0500

    Added info in Config File so easy to setup!

--- cfg.lua
+++ cfg.lua
@@ -1,5 +1,6 @@
 Config = {}
 
-Config.ShowIdOnHead = true
-Config.SteamHex = false
-Config.ShowName = true
+Config.ShowIdOnHead = true -- True to see the ID's on Head and False for Obviously not seeing the name.
+Config.SteamHex = false -- You should have apikey set in server.cfg if this is true
+Config.ShowName = true -- Should the User Info Show Names too? False For Not Seeing the Name.
+Config.UseEmotes = true -- Your Server Should have /e phone command, change phone to whatever emote you want to change at cl_scoreboard.lua at line:237.
